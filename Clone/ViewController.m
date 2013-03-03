3ed0b8328b7abffc48fa5a6490256a3bd192997e
David SK app developer <david@smarterkey.com>
2013-03-03 12:07:39 +0100

    Clone1 added

--- Clone/ViewController.m
+++ Clone/ViewController.m
@@ -24,6 +24,7 @@
 {
     [super viewDidUnload];
     // Release any retained subviews of the main view.
+    // Clone1
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
