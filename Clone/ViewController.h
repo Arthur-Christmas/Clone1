da0457940ce4aff4489328bcce002408dfd98c5c
David SK app developer <david@smarterkey.com>
2013-03-03 11:58:54 +0100

    Initial Commit

---
+++ Clone/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Clone
+//
+//  Created by Administrator on 3/3/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
